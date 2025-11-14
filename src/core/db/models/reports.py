d68604f004602e2ac2dceb4ab2a2887a114ce681
fazzilka <fazzil349@gmail.com>
2025-11-14 17:04:09 +0300

    feat(reports): добавить модель таблицы отчетов по проектам

--- src/core/db/models/reports.py
+++ src/core/db/models/reports.py
@@ -2,33 +2,35 @@ from __future__ import annotations
 
 from uuid import UUID, uuid4
 
-from sqlalchemy import Boolean, DateTime, ForeignKey, String, Text, func
+from sqlalchemy import DateTime, ForeignKey, String, Text, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ..base import Base
 
 
-class Project(Base):
-    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
-
-    teamlead_id: Mapped[UUID] = mapped_column(
-        ForeignKey("users.id", ondelete="RESTRICT"), nullable=False
-    )
-    curator_id: Mapped[UUID] = mapped_column(
-        ForeignKey("users.id", ondelete="RESTRICT"), nullable=False
+class Report(Base):
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True,
+        default=uuid4,
     )
 
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    file_url: Mapped[str] = mapped_column(Text, nullable=False)
+
+    creator_id: Mapped[UUID] = mapped_column(
+        ForeignKey("users.id", ondelete="RESTRICT"),
+        nullable=False,
+    )
+    editor_id: Mapped[UUID | None] = mapped_column(
+        ForeignKey("users.id", ondelete="SET NULL"),
+    )
+
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
     description: Mapped[str | None] = mapped_column(Text)
-
-    is_completed: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default="false")
-
-    avatar_url: Mapped[str | None] = mapped_column(Text)
-    github_url: Mapped[str] = mapped_column(Text, nullable=False, server_default="")
+    teacher_note: Mapped[str | None] = mapped_column(Text)
 
     created_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=func.now()
     )
-    updated_at: Mapped[DateTime] = mapped_column(
-        DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now()
+    updated_at: Mapped[DateTime | None] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
