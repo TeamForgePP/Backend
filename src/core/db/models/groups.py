1934b68b9c4795f0475a58b7e16b5c49bb13af39
fazzilka <fazzil349@gmail.com>
2025-11-14 16:24:52 +0300

    feat(groups): добавить модель таблицы учебных групп

--- src/core/db/models/groups.py
+++ src/core/db/models/groups.py
@@ -1,33 +1,38 @@
 from __future__ import annotations
 
-from uuid import uuid4
+from uuid import UUID, uuid4
 
-from sqlalchemy import String, SmallInteger, Integer, DateTime, ForeignKey, Enum, func
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import DateTime, Enum, ForeignKey, Integer, SmallInteger, String, func
+from sqlalchemy.orm import Mapped, mapped_column
 
 from ..base import Base
-from ..enums import LevelEducation, Faculty
+from ..enums import Faculty, LevelEducation
 
 
 class Group(Base):
-    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
 
-    name: Mapped[str] = mapped_column(String(37), nullable=False)
+    name: Mapped[str | None] = mapped_column(String(11))
 
     level_ed: Mapped[LevelEducation] = mapped_column(
         Enum(LevelEducation, name="level_education", create_constraint=True), nullable=False
     )
-    faculty: Mapped[Faculty] = mapped_column(Enum(Faculty, name="faculty", create_constraint=True), nullable=False)
 
-    year_adm: Mapped[int | None] = mapped_column(SmallInteger)
-    curator_id: Mapped[UUID] = mapped_column(ForeignKey("users.id", ondelete="RESTRICT"), nullable=False)
-    students: Mapped[int | None] = mapped_column(Integer)
+    faculty: Mapped[Faculty] = mapped_column(
+        Enum(Faculty, name="faculty", create_constraint=True), nullable=False
+    )
 
-    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    year_adm: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+
+    curator_id: Mapped[UUID] = mapped_column(
+        ForeignKey("users.id", ondelete="RESTRICT"), nullable=False
+    )
+
+    students: Mapped[int] = mapped_column(Integer, nullable=False, server_default="0")
+
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
     updated_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now()
     )
-
-    curator: Mapped["User"] = relationship(back_populates="curated_groups")
-    members: Mapped[list["User"]] = relationship(back_populates="group")
