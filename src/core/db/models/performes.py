335a08630ed53586cef532dee843ced6f86aab1e
jujunkaa <jujunka086@gmail.com>
2025-11-15 23:41:26 +0300

    feat(models): добавить модель таблицы исполнителей тасков

--- src/core/db/models/performes.py
+++ src/core/db/models/performes.py
@@ -1,8 +1,19 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from uuid import UUID as pyUUID
+
 from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
-from src.core.db.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ..base import Base
+
 
 class Performes(Base):
-    task_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("tasks.id"), nullable=False)
-    #assigne_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    task_id: Mapped[pyUUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("tasks.id"), nullable=False
+    )
+
+
+#   assigne_id: Mapped[pyUUID] = mapped_column(
+#       UUID(as_uuid=True),
+#       ForeignKey("users.id"),
+#       nullable=False)
