f3babfae963943d953b946a652fc6aa3369c84d3
jujunkaa <jujunka086@gmail.com>
2025-11-15 23:58:31 +0300

    feat(models): добавить модель таблицы для ролей в проекте

--- src/core/db/models/project_role.py
+++ src/core/db/models/project_role.py
@@ -1,18 +1,21 @@
+# from uuid import UUID as pyUUID
+
 from sqlalchemy import Enum
 from sqlalchemy.orm import Mapped, mapped_column
 
-from src.core.db.base import Base
-
+from ..base import Base
 from .enum import TeamRole
 
 
 class ProjectRole(Base):
-    # project_id: Mapped[UUID] = mapped_column(
+    # project_id: Mapped[pyUUID] = mapped_column(
     #   UUID(as_uuid=True),
     #   ForeignKey("projects.id"),
     #   nullable=False)
-    # user_id: Mapped[UUID] = mapped_column(
+
+    # user_id: Mapped[pyUUID] = mapped_column(
     #   UUID(as_uuid=True),
     #   ForeignKey("users.id"),
     #   nullable=False)
+
     role: Mapped[TeamRole] = mapped_column(Enum(TeamRole), nullable=False)
