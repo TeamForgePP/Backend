9a588d67d9f6c6b12d38be18f19c51174faac10e
jujunkaa <jujunka086@gmail.com>
2025-11-15 23:29:40 +0300

    feat(models): добавить модель таблицы приглашений

--- src/core/db/models/invitations.py
+++ src/core/db/models/invitations.py
@@ -1,20 +1,33 @@
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Enum, ForeignKey, DateTime, text
-from sqlalchemy.dialects.postgresql import UUID
-from enum import Enum as Enumm
+from uuid import UUID as pyUUID
 from uuid import uuid4
-from src.core.db.base import Base
 
-class InvitationStatus(Enumm):
-    Posted = "posted"
-    Rejected = "rejected"
-    Accepted = "accepted"
+from sqlalchemy import DateTime, Enum, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ..base import Base
+from .enum import InvitationStatus
+
 
 class Invitations(Base):
-    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    #notification_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("notifications.id"), nullable=False)
-    #project_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("projects.id"), nullable=False)
-    #invited_user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
-    #invited_by_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
-    status: Mapped[InvitationStatus] = mapped_column(Enum(InvitationStatus), default=text("PENDING"))
-    created_at: Mapped[DateTime] = mapped_column(DateTime, nullable=False, default=text("now()"))
+    id: Mapped[pyUUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    # notification_id: Mapped[pyUUID] = mapped_column(
+    #   UUID(as_uuid=True),
+    #   ForeignKey("notifications.id"),
+    #   nullable=False)
+    # project_id: Mapped[pyUUID] = mapped_column(
+    #   UUID(as_uuid=True),
+    #   ForeignKey("projects.id"),
+    #   nullable=False)
+    # invited_user_id: Mapped[pyUUID] = mapped_column(
+    #   UUID(as_uuid=True),
+    #   ForeignKey("users.id"),
+    #   nullable=False)
+    # invited_by_id: Mapped[pyUUID] = mapped_column(
+    #   UUID(as_uuid=True),
+    #   ForeignKey("users.id"),
+    #   nullable=False)
+    status: Mapped[InvitationStatus] = mapped_column(Enum(InvitationStatus))
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now()
+    )
