7b7c0f17523709d881b9c8c6f4e3943ec06b2d64
fazzilka <fazzil349@gmail.com>
2025-12-12 01:01:48 +0300

    feat(auth): подключить роутер авторизации пользователя [KAN-46]

--- src/modules/router.py
+++ src/modules/router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from src.modules.admin.routers import user_router
 from src.modules.auth.routers.admin import router as admin_auth_router
+from src.modules.auth.routers.user import router as user_auth_router
 
 router = APIRouter(prefix="/api")
 router.include_router(admin_auth_router)
 router.include_router(user_router)
+router.include_router(user_auth_router)
