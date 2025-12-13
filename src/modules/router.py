d8cc3a1334ff7f491baae48d0e0cce13cd892d67
jujunkaa <jujunka086@gmail.com>
2025-12-13 16:35:51 +0300

    feat(router): подключить роутер модуля home

--- src/modules/router.py
+++ src/modules/router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from src.modules.admin.routers import user_router
 from src.modules.auth.routers.admin import router as admin_auth_router
+from src.modules.home.routers.home_router import router as home_router
 
 router = APIRouter(prefix="/api")
 router.include_router(admin_auth_router)
 router.include_router(user_router)
+router.include_router(home_router)
