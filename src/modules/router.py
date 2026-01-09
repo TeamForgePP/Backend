47a2a41e98541ff4a2a619e610b226a23768df0c
jujunkaa <jujunka086@gmail.com>
2026-01-09 19:07:22 +0300

    feat(router): подключить роутер модуля kanban

--- src/modules/router.py
+++ src/modules/router.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter
 
 from src.modules.admin.routers import user_router
 from src.modules.auth.routers.admin import router as admin_auth_router
-from src.modules.home.routers.home_router import router as home_router
 from src.modules.auth.routers.user import router as user_auth_router
+from src.modules.home.routers.home_router import router as home_router
+from src.modules.kanban.routers.kanban_router import router as kanban_router
 
 router = APIRouter(prefix="/api")
 router.include_router(admin_auth_router)
 router.include_router(user_router)
 router.include_router(home_router)
 router.include_router(user_auth_router)
+router.include_router(kanban_router)
