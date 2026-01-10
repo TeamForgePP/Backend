33108c77ad728d37fb7d0807d8f75a62a64d0536
fazzilka <fazzil349@gmail.com>
2026-01-10 19:50:44 +0300

    feat(profile): добавить схемы профиля [KAN-52]

--- src/modules/profile/schemas/profile.py
+++ src/modules/profile/schemas/profile.py
@@ -1,19 +1,18 @@
+from uuid import UUID
+
 from pydantic import BaseModel, EmailStr
 
 
 class ProfileResponse(BaseModel):
     full_name: str
     group: str | None
-    role: str
+    role: str | None
     teams: list[str]
     email: EmailStr
 
 
 class ProfileUpdateRequest(BaseModel):
-    last_name: str | None = None
     first_name: str | None = None
+    last_name: str | None = None
     patronymic: str | None = None
-    email: EmailStr | None = None
-
-    class Config:
-        extra = "forbid"
+    group_id: UUID | None = None
