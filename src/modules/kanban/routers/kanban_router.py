1fc5fe2e99fea4c3717b029802f7cb86e2a3c8fd
yurtsev <yurtsevburiy@gmail.com>
2026-01-20 13:59:48 +0300

    fix(kanban): устранил конфликт роутов и 422 при запросе team-members

--- src/modules/kanban/routers/kanban_router.py
+++ src/modules/kanban/routers/kanban_router.py
@@ -29,7 +29,27 @@ async def get_kanban_info(_user: PrincipalContext = user_dep) -> KanbanResponse:
 
 
 @router.get(
-    "/{sprint_id}",
+    "/team-members",
+    response_model=MembersResponse,
+    status_code=status.HTTP_200_OK,
+    name="get_all_team_members",
+)
+async def get_all_team_members(_user: PrincipalContext = user_dep) -> MembersResponse:
+    return await KanbanService.get_all_team_members(_user.sub, _user.role)
+
+
+@router.get(
+    "/sprints",
+    response_model=SprintsResponse,
+    status_code=status.HTTP_200_OK,
+    name="get_number_of_sprints",
+)
+async def get_number_of_sprints(_user: PrincipalContext = user_dep) -> SprintsResponse:
+    return await KanbanService.get_number_of_sprints(_user.sub, _user.role)
+
+
+@router.get(
+    "/sprints/{sprint_id}",
     response_model=KanbanResponse,
     status_code=status.HTTP_200_OK,
     name="get_kanban_by_sprint_id",
@@ -41,6 +61,16 @@ async def get_kanban_by_sprint_id(
     return await KanbanService.get_kanban_info_by_sprint_id(_user.sub, _user.role, sprint_id)
 
 
+@router.get(
+    "/tasks/{task_id}",
+    response_model=TaskResponse,
+    status_code=status.HTTP_200_OK,
+    name="get_task",
+)
+async def get_task(task_id: UUID, _user: PrincipalContext = user_dep) -> TaskResponse:
+    return await KanbanService.get_task(task_id)
+
+
 @router.post(
     "/new-task",
     response_model=BasicResponse,
@@ -49,36 +79,6 @@ async def get_kanban_by_sprint_id(
 )
 async def create_task(data: NewTaskRequest, _user: PrincipalContext = user_dep) -> BasicResponse:
     return await KanbanService.create_task(data, _user.sub, _user.role)
-
-
-@router.get(
-    "/sprints",
-    response_model=SprintsResponse,
-    status_code=status.HTTP_200_OK,
-    name="get number of sprints",
-)
-async def get_number_of_sprints(_user: PrincipalContext = user_dep) -> SprintsResponse:
-    return await KanbanService.get_number_of_sprints(_user.sub, _user.role)
-
-
-@router.get(
-    "/team-members",
-    response_model=MembersResponse,
-    status_code=status.HTTP_200_OK,
-    name="get_all_team_members",
-)
-async def get_all_team_members(_user: PrincipalContext = user_dep) -> MembersResponse:
-    return await KanbanService.get_all_team_members(_user.sub, _user.role)
-
-
-@router.get(
-    "/{task_id}",
-    response_model=TaskResponse,
-    status_code=status.HTTP_200_OK,
-    name="get_task",
-)
-async def get_task(task_id: UUID, _user: PrincipalContext = user_dep) -> TaskResponse:
-    return await KanbanService.get_task(task_id)
 
 
 @router.post(
