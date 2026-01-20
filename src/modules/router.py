b8aec73c4c2cd6f0e83df2a65b936c8078ac09de
fazzilka <fazzil349@gmail.com>
2026-01-20 23:43:27 +0300

    feat(project): добавить ручку invitations [KAN-51]

--- src/modules/router.py
+++ src/modules/router.py
@@ -4,8 +4,8 @@ from src.modules.admin.routers import group_router, user_router
 from src.modules.auth.routers.admin import router as admin_auth_router
 from src.modules.auth.routers.user import router as user_auth_router
 from src.modules.home.routers.home_router import router as home_router
+from src.modules.invitations.routers.invitations_router import router as invitations_router
 from src.modules.kanban.routers.kanban_router import router as kanban_router
-from src.modules.notifications.routers.invitation_router import router as invitation_router
 from src.modules.notifications.routers.notifications_router import router as notifications_router
 from src.modules.project.routers.project_router import router as project_router
 from src.modules.sprints.routers.sprints_router import router as sprints_router
@@ -19,5 +19,5 @@ router.include_router(user_auth_router)
 router.include_router(sprints_router)
 router.include_router(kanban_router)
 router.include_router(notifications_router)
-router.include_router(invitation_router)
+router.include_router(invitations_router)
 router.include_router(project_router)
