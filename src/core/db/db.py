e107ce6326962500eb17a0d3fb9b3a5a3a4ec4ed
yurtsev <yurtsevburiy@gmail.com>
2026-01-22 00:14:29 +0900

    refactor(db): обновил инициализацию и сессию

--- src/core/db/db.py
+++ src/core/db/db.py
@@ -2,11 +2,16 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from src.config import cfg
 
-async_engine = create_async_engine(cfg.database.async_database_url, echo=False)
+engine = create_async_engine(
+    cfg.database.async_database_url,
+    echo=False,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+)
 
 Session = async_sessionmaker(
+    bind=engine,
     autocommit=False,
     autoflush=False,
-    bind=async_engine,
     expire_on_commit=False,
 )
--- src/core/db/session.py
+++ src/core/db/session.py
@@ -1,33 +1,16 @@
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.config import cfg
 from src.core.db.db import Session
 
 
 @asynccontextmanager
 async def get_session() -> AsyncIterator[AsyncSession]:
-    session = Session()
-    try:
-        yield session
-        await session.commit()
-    except Exception:
-        await session.rollback()
-        raise
-    finally:
-        await session.close()
-
-
-engine = create_async_engine(
-    cfg.database.async_database_url,
-    echo=False,
-    future=True,
-)
-
-async_session_maker = async_sessionmaker(
-    engine,
-    expire_on_commit=False,
-    class_=AsyncSession,
-)
+    async with Session() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
