7398dd46b3beb00dd10d91182f1b1caadfb14cf2
yurtsev <yurtsevburiy@gmail.com>
2026-01-22 17:21:46 +0900

    feat(docker)!: подготовили docker-compose для деплоя

--- src/modules/router.py
+++ src/modules/router.py
@@ -7,8 +7,8 @@ from src.modules.home.routers.home_router import router as home_router
 from src.modules.kanban.routers.kanban_router import router as kanban_router
 from src.modules.notifications.routers.invitation_router import router as invitation_router
 from src.modules.notifications.routers.notifications_router import router as notifications_router
-from src.modules.project.routers.project_router import router as project_router
 from src.modules.profile.routers.profile_router import router as profile_router
+from src.modules.project.routers.project_router import router as project_router
 from src.modules.sprints.routers.sprints_router import router as sprints_router
 
 router = APIRouter(prefix="/api")
